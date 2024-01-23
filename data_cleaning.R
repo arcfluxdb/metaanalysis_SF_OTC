f6af78c22291005153b4e73f185dbc7c71c23b1a
Jan Dietrich <jandietrich67@gmail.com>
2024-01-23 17:14:28 +0100

    this is the setup up for cleaning the data

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,1 +1,9 @@
-#
+# 2024.01.23
+# Jan, Sarah
+
+library(readr)
+
+full_data <- read_csv("full_data_wrong_units.csv", 
+                                  col_types = cols(...1 = col_skip(), flux_date = col_date(format = "%d/%m/%Y")))
+
+
