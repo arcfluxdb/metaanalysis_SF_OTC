f8f32290f0ba2b168fd4dc54e0b660928eefc9e6
SarahSchwieger <sarah.schwieger@umu.se>
2024-01-26 11:35:07 +0100

    Test

--- data_cleaning.R
+++ data_cleaning.R
@@ -6,4 +6,4 @@ library(readr)
 full_data <- read_csv("full_data_wrong_units.csv", 
                                   col_types = cols(...1 = col_skip(), flux_date = col_date(format = "%d/%m/%Y")))
 
-
+#Test test
