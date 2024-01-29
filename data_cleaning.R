a76e86562998855442167dab03bc7a90113ee39b
Jan Dietrich <jandietrich67@gmail.com>
2024-01-29 11:52:05 +0100

    quick fixes, liberary load in first script

--- data_cleaning.R
+++ data_cleaning.R
@@ -2,9 +2,17 @@
 # Jan, Sarah
 
 library(readr)
+library(tidyverse)
+
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(effsize)
+library(esc)
+library(metafor)
+library(lme4)
 
 full_data <- read_csv("full_data_wrong_units.csv", 
                                   col_types = cols(...1 = col_skip(), flux_date = col_date(format = "%d/%m/%Y")))
 
-#Test test
 
