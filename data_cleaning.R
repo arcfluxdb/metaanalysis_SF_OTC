45ddc6b09afa3dbbe20854a2c5260a737beb8235
Sarah Schwieger <sarah.schwieger@umu.se>
2024-01-31 14:10:06 +0100

    renamed "Toolik MAT SF" to ALA_13

--- data_cleaning.R
+++ data_cleaning.R
@@ -16,3 +16,4 @@ full_data <- read_csv("full_data_wrong_units.csv",
                                   col_types = cols(...1 = col_skip(), flux_date = col_date(format = "%d/%m/%Y")))
 
 
+full_data$site_id <- recode(full_data$site_id, "Toolik MAT SF" = "ALA_13")
