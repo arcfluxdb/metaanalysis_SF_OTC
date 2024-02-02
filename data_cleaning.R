402727286ceb1901cd2a27ac9c95702728bd8855
Jan Dietrich <jandietrich67@gmail.com>
2024-02-02 14:08:59 +0100

    working data name changed

--- data_cleaning.R
+++ data_cleaning.R
@@ -14,24 +14,34 @@ library(metafor)
 library(lme4)
 
 
-full_data_wrong_units <- read_csv("full_data_wrong_units.csv", 
-                                  col_types = cols(...1 = col_skip(), flux_date = col_date(format = "%d/%m/%Y")))
+#full_data_wrong_units <- read_csv("full_data_wrong_units.csv", 
+#                                  col_types = cols(...1 = col_skip(), flux_date = col_date(format = "%d/%m/%Y")))
+
+
 
 all_data_230823 <- readRDS(file = "flux_db_FULL_230823.rds")
 
-# change date column
+working_data <- all_data_230823 %>% 
+  filter(treatment %in% c("CTL","OTC","SNOWFENCE","OTCxSNOWFENCE")) %>% 
+  filter(site_id != "AUS_1")  
+  
+working_data$site_id <- recode(working_data$site_id, "Toolik MAT SF" = "ALA_13")
 
-all_dates <- all_data_230823 %>% 
-  group_by(site_id, year) %>% 
-  dplyr::summarise()
+rm(all_data_230823)
 
-
-all_dates <- all_data_230823 %>%
-  mutate(flux_date = parse_date_time(flux_date, orders = c("ymd", "mdy", "dmy")))
-
-
-View(all_data_230823[all_data_230823$site_id=="ALA_1",])
-
-full_data$site_id <- recode(full_data$site_id, "Toolik MAT SF" = "ALA_13")
-
-full_data$site_id <- recode(full_data$site_id, "Toolik MAT SF" = "ALA_13")
+# # change date column
+# 
+# all_dates <- all_data_230823 %>% 
+#   group_by(site_id, year) %>% 
+#   dplyr::summarise()
+# 
+# 
+# all_dates <- all_data_230823 %>%
+#   mutate(flux_date = parse_date_time(flux_date, orders = c("ymd", "mdy", "dmy")))
+# 
+# 
+# View(all_data_230823[all_data_230823$site_id=="ALA_1",])
+# 
+# full_data$site_id <- recode(full_data$site_id, "Toolik MAT SF" = "ALA_13")
+# 
+# full_data$site_id <- recode(full_data$site_id, "Toolik MAT SF" = "ALA_13")
