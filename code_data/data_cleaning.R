7833ecc5d12ba6c8832c8a8ddab5695db745dc96
Sarah Schwieger <sarah.schwieger@umu.se>
2024-07-31 13:48:25 +0200

    working directory does not work for me. Copied this part of the script in the other script

--- code_data/data_cleaning.R
+++ code_data/data_cleaning.R
@@ -16,9 +16,7 @@ library(lme4)
 #full_data_wrong_units <- read_csv("full_data_wrong_units.csv", 
 #                                  col_types = cols(...1 = col_skip(), flux_date = col_date(format = "%d/%m/%Y")))
 
-
 all_data <- readRDS("D:/flux_db/metaanalysis_SF_OTC/database/database.rds")$fluxdata
-
 
 unique(all_data$site_id_automatic)
 
